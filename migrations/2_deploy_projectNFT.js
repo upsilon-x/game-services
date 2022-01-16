498ff323c9e2491ed9c4428145892b01e7cc61f9
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-01-15 19:34:33 -0500

    added multicall

--- migrations/2_deploy_projectNFT.js
+++ migrations/2_deploy_projectNFT.js
@@ -1,5 +1,10 @@
 const ProjectNFT = artifacts.require("ProjectNFT");
+const UpdatedMulticall = artifacts.require("UpdatedMulticall");
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network) {
   deployer.deploy(ProjectNFT);
+
+  if (network == "development") {
+    deployer.deploy(UpdatedMulticall);
+  }
 };
