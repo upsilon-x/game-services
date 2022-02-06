29acd50edb8e5dc7df48151f7372e4a4a26d1654
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-02-05 20:15:03 -0600

    deploy achievemen-ts

--- contracts/project-control/ProjectAccess.sol
+++ contracts/project-control/ProjectAccess.sol
@@ -6,15 +6,6 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
  * The default way of defining project access.
  */
 contract ProjectAccess is AccessControl {
-    function hasRole(bytes32 role, address addr)
-        public
-        view
-        override
-        returns (bool)
-    {
-        return super.hasRole(role, addr);
-    }
-
     constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
     }
--- migrations/3_deploy_achievements.js
+++ migrations/3_deploy_achievements.js
@@ -1,7 +1,7 @@
 const ProjectNFT = artifacts.require("ProjectNFT");
 const AchievementAggregator = artifacts.require("AchievementAggregator");
 
-module.exports = function (deployer) {
+module.exports = async function (deployer) {
   let nft = await ProjectNFT.deployed();
   await deployer.deploy(AchievementAggregator, nft.address);
 };
